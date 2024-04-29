4ab93af63681a53bf93c90689db721fc8c6164d4
Rix the Tyrunt <rixthetyrunt@gmail.com>
2024-04-29 20:23:23 +0300

    balancing out the Bash command too

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo NOTE: the gradOS.js file this command generates is not the JS you should be using. Read the README from more info.
-emcc gradOS.c -Os -g2 -sWASM=1 -sIMPORTED_MEMORY=1 -o gradOS.js -sEXPORTED_FUNCTIONS=_malloc && rm gradOS.js
+emcc gradOS.c -Os -g2 -sWASM=1 -sIMPORTED_MEMORY=1 -o gradOS.js -sEXPORTED_FUNCTIONS=_malloc
